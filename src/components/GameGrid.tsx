634ed1e8d02a837b4d2f4b77d598f6066edb6344
Adi <asanand1297@gmail.com>
2024-10-29 18:54:34 +0530

    Refractor Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,11 +13,9 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-
+  if (error) return <Text>{error}</Text>;
   return (
     <>
-      {error && <Text color="red.500">{error}</Text>}
-
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
