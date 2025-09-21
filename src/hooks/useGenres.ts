823ca4453be7222c92d211a3e641e7f157102a1c
adi-ensei <your_github_email@example.com>
2025-09-21 17:41:30 +0530

    Refractor- create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
 
 import APIClient from "../services/api-client";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -16,6 +16,6 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: genres.length, results: genres },
+    initialData: genres,
   });
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import APIClient from "../services/api-client";
 import platforms from "../data/platforms";
+
 export interface Platform {
   id: number;
   name: string;
@@ -15,7 +16,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: platforms.length, results: platforms },
+    initialData: platforms,
   });
 
 export default usePlatforms;
