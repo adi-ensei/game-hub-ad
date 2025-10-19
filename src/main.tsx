d4a45b24dc8ea1b275dec378f4b9c10a0301eb67
adi-ensei <your_github_email@example.com>
2025-10-20 00:25:15 +0530

    Hanndeled errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import theme from "./Theme.tsx";
-import App from "./App.tsx";
+
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +16,7 @@ createRoot(document.getElementById("root")!).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.ColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
